7d9d9c6bd778b9e482dfede54470fd1513b5c472
marksre <36451098+marksre@users.noreply.github.com>
2020-08-06 12:27:32 -0400

    Modified pipeline and visualization for RC project

---
+++ Step1_NIRStoolbox_pipeline_RAM080420.m
@@ -0,0 +1,115 @@
+%% Load data
+datadir=uigetdir();
+
+disp('Loading in .nirs data files...')
+raw = nirs.io.loadDirectory(datadir, {'Subject'});
+% raw = nirs.io.loadDirectory(datadir, {'Subject','Task'});
+disp('All .nirs files loaded!')
+disp('-----------------------')
+
+% set the duration to be 6 sec
+for i=1:length(raw)
+    for j=1:length(raw(i).stimulus.values{1}.dur)
+        raw(i).stimulus.values{1}.dur(j)=6;
+    end
+    for j=1:length(raw(i).stimulus.values{2}.dur)
+        raw(i).stimulus.values{2}.dur(j)=6;
+    end
+    for j=1:length(raw(i).stimulus.values{3}.dur)
+        raw(i).stimulus.values{3}.dur(j)=6;
+    end
+end
+
+%% First Level Analysis
+disp('Running data resample...')
+resample=nirs.modules.Resample();
+resample.Fs=5;
+downraw=resample.run(raw);
+
+disp('Converting Optical Density...')
+odconv=nirs.modules.OpticalDensity();
+od=odconv.run(downraw);
+
+disp('Applying  Modified Beer Lambert Law...')
+mbll=nirs.modules.BeerLambertLaw();
+hb=mbll.run(od);
+
+disp('Trimming .nirs files...')
+trim=nirs.modules.TrimBaseline();
+trim.preBaseline=5;
+trim.postBaseline=5;
+hb_trim=trim.run(hb);
+
+%% Visual Check of Stim Marks
+figure
+for i=1:length(hb_trim)
+    load(hb_trim(i).description,'-mat');
+    subplot(10,10,i);
+    plot(s);
+end
+
+disp('Processing complete!')
+disp('-----------------------')
+
+%% Subject Level Modeling
+disp('Now running subject-level GLM!')
+firstlevelglm=nirs.modules.AR_IRLS();
+firstlevelbasis = nirs.design.basis.Canonical();
+disp('Initial GLM complete')
+
+% Adding temporal & dispersion derivatives to canonical HRF function, DCT matrix to account for signal drift over time
+firstlevelbasis.incDeriv=1;
+firstlevelglm.trend_func=@(t) nirs.design.trend.dctmtx(t,0.008);
+disp('Added DCT matrix + 2 derivatives')
+
+% HRF peak time = 6s based on Friederici and Booth papers (e.g. Brauer, Neumann & Friederici, 2008, NeuroImage)
+firstlevelbasis.peakTime = 6;
+firstlevelglm.basis('default') = firstlevelbasis;
+disp('Peak time set at 6s')
+
+tic
+SubjStats=firstlevelglm.run(hb_trim);
+disp('Ready to save SubjStats...')
+toc
+
+%save('SubjStats')
+disp('Done!')
+
+%% Demographics Behavioral Correlation
+Demo = nirs.modules.AddDemographics();
+Demo.demoTable = readtable('/Users/marksre/Desktop/RCinDyslexia/BxData/BehavioralVariables_N77.xlsx');
+Demo.varToMatch='Subject';
+SubjStats = Demo.run(SubjStats);
+
+%% Group Level Analysis
+% Run GLM with SEPARATE conditions and NO REGRESSORS. Only TASK (conditions) vs. REST
+% Interaction between task and condition (compare two tasks in one model)
+% tic
+% disp('Running GroupStats1 GLM')
+% grouplevelpipeline=nirs.modules.MixedEffects();
+% grouplevelpipeline.formula ='beta ~ -1 + Task*cond + ppvt + age + ses + (1|Subject)';
+% GroupStats1 = grouplevelpipeline.run(SubjStats);
+% disp('GroupStats done!')
+% toc
+
+%% MA & PA separate group level analysis 
+% N80MASubjStats=SubjStats(1:2:end-1);
+% N80PASubjStats=SubjStats(2:2:end);
+
+tic
+disp('Running GroupStats GLM #1')
+grouplevelpipeline1=nirs.modules.MixedEffects();
+grouplevelpipeline1.formula ='beta ~ -1 + cond + age + ses + (1|Subject)';
+% grouplevelpipeline1.formula ='beta ~ -1 + cond + (1|Subject)';
+GroupStats1 = grouplevelpipeline1.run(SubjStats);
+disp('GroupStats done!')
+toc
+
+tic
+disp('Running GroupStats GLM #2 - including passage comp')
+grouplevelpipeline2=nirs.modules.MixedEffects();
+grouplevelpipeline2.formula ='beta ~ -1 + cond + pc + age + ses + (1|Subject)';
+% grouplevelpipeline2.formula ='beta ~ -1 + cond + (1|Subject)';
+GroupStats2 = grouplevelpipeline2.run(SubjStats);
+disp('GroupStats done!')
+toc
---
+++ Step3_GroupLevelPlot_RAM080420.m
@@ -0,0 +1,119 @@
+%% Contrasts controlling for age & maternal education
+c1MA=[1 0 0 1 0 0 1 0 0 0 0]; %3 conditions
+c2MA=[1 0 0 0 0 0 0 0 0 0 0]; %easy
+c3MA=[0 0 0 1 0 0 0 0 0 0 0]; %hard
+c4MA=[0 0 0 0 0 0 1 0 0 0 0]; %control
+
+c5MA=[1 0 0 1 0 0 0 0 0 0 0]; %task > rest
+c6MA=[1 0 0 1 0 0 -1 0 0 0 0]; %task > control
+c7MA=[1 0 0 0 0 0 -1 0 0 0 0]; %easy > control
+c8MA=[0 0 0 1 0 0 -1 0 0 0 0]; %hard > control
+
+ageMA=[0 0 0 0 0 0 0 0 0 1 0]; %age only
+sesMA=[0 0 0 0 0 0 0 0 0 0 1]; %maternal ed only
+
+
+[intensity1,p1]=getintensity(c1MA,GroupStats1);
+[intensity2,p2]=getintensity(c2MA,GroupStats1);
+[intensity3,p3]=getintensity(c3MA,GroupStats1);
+[intensity4,p4]=getintensity(c4MA,GroupStats1);
+[intensity5,p5]=getintensity(c5MA,GroupStats1);
+[intensity6,p6]=getintensity(c6MA,GroupStats1);
+[intensity7,p7]=getintensity(c7MA,GroupStats1);
+[intensity8,p8]=getintensity(c8MA,GroupStats1);
+[intensity9,p9]=getintensity(ageMA,GroupStats1);
+[intensity10,p10]=getintensity(sesMA,GroupStats1);
+
+%% Plot the 3D image
+onlypositive=0;
+
+%% Figure 1: All task conditions
+figure
+subplot(2,2,1)
+plot(intensity1,onlypositive,p1);
+title('MA All')
+
+subplot(2,2,2)
+plot(intensity2,onlypositive,p2);
+title('Easy')
+
+subplot(2,2,3)
+plot(intensity3,onlypositive,p3);
+title('Hard')
+
+subplot(2,2,4)
+plot(intensity14,onlypositive,p14);
+title('Control')
+
+%% Figure 2: Contrasts between conditions
+figure
+subplot(2,2,1)
+plot(intensity5,onlypositive,p5);
+title('Easy+Hard > Rest')
+
+subplot(2,2,2)
+plot(intensity6,onlypositive,p6);
+title('Easy+Hard > Control')
+
+subplot(2,2,3)
+plot(intensity7,onlypositive,p7);
+title('Easy > Control')
+
+subplot(2,2,4)
+plot(intensity8,onlypositive,p8);
+title('Hard > Control')
+
+%% Figure 3: Covariates of no interest
+figure
+subplot(2,1,1);
+plot(intensity9,onlypositive,p9)
+title('Age')
+
+subplot(2,1,2);
+plot(intensity10,onlypositive,p10)
+title('MatEd')
+
+
+%% Functions
+function [intensity,p] = getintensity(c,GroupStats)
+Contrast=GroupStats.ttest(c);
+Contrasttable=Contrast.table;
+% intensity=Contrasttable.tstat(strcmp(Contrasttable.type,'hbo')&ismember(Contrasttable.source,[1 2 3 4 5 6 7 8]));
+intensity=Contrasttable.tstat(strcmp(Contrasttable.type,'hbo'));
+p=Contrasttable.p(strcmp(Contrasttable.type,'hbo'));
+end
+
+function plot(intensity,onlypositive,p)
+load CHMNI_Bilateral46_AUG2020.mat % Load Coordinates - Updated coordinates on Aug 2020
+    % MNIcoordUnilateral23_AUG2020: Left hemisphere, removed channels 7 & 8 
+    % Localization fixed August 2020, all coordinates shifted down slightly
+mx=4;
+mn=-4;
+
+% remove the negative intensity associated ind
+if onlypositive
+    negind=find(intensity<=0);
+else
+    negind=[];
+end
+
+insigind=find(p>=0.05);
+
+
+if ~isempty(negind)
+    rind=unique([negind insigind]);
+else
+    rind=insigind;
+end
+
+intensity(rind)=[];
+MNIcoordBi(rind,:)=[];
+
+MNIcoordstd=10*ones(length(MNIcoordBi));
+
+Plot3D_channel_registration_result(intensity, MNIcoordBi, MNIcoordstd,mx,mn);
+
+camlight('headlight','infinite');
+lighting gouraud
+material dull;
+end
